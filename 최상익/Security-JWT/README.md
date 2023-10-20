493a86f8262c112c8f3306add6e510efbcc0b14a
권인식 <kwon2566@naver.com>
2023-10-20 14:47:29 +0900

    Remove: front 디렉토리 정리

--- 최상익/Security-JWT/README.md
+++
@@ -1,7 +0,0 @@
-# Security-JWT
-
-- Spring Security
-
-- OAuth2 (Google, Kakao, Naver)
-
-- JWT (Access Token + Refresh Token / Redis)
--- 최상익/Security-JWT/src/main/java/com/example/securitystudy/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.securitystudy.user.repository;
-
-import com.example.securitystudy.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
