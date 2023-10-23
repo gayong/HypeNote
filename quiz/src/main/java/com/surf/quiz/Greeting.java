ead9e08f43bc2998431f174e60b4ceccaf57735c
최상익 <qjxltm@naver.com>
2023-10-23 10:52:30 +0900

    fix: 설정 변경
    quiz yaml 및 config 설정 변경

---
+++ quiz/src/main/java/com/surf/quiz/Greeting.java
@@ -0,0 +1,12 @@
+package com.surf.quiz;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+    private String content;
+}
