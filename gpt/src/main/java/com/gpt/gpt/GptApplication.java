69ca07b5f2d5b639e6f090298c14ac4c4a81cb80
권인식 <kwon2566@naver.com>
2023-10-23 11:48:27 +0900

    Feat: GPT Server Project Start

---
+++ gpt/src/main/java/com/gpt/gpt/GptApplication.java
@@ -0,0 +1,13 @@
+package com.gpt.gpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptApplication.class, args);
+	}
+
+}
