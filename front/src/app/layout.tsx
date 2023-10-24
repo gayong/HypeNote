13cf0038fc2b416df1804305d0cc47363ccb0127
윤자현 <bajh323@naver.com>
2023-10-24 17:37:46 +0900

    Refactor: 로그인폼 atmoic구조로 분해

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -6,7 +6,10 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "한입노트(Hype Note)",
+  title: {
+    default: "한입노트(Hype Note)",
+    template: "한입노트(Hype Note)",
+  },
   description: "한번에 모든 걸 해결 할 수 있는 나만의 한입 노트!",
 };
 
