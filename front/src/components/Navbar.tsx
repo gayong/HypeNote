6a3b09812b5f1f6f371a2b7e620e2dff6a001d8d
이가영 <lgy4625@naver.com>
2023-10-25 15:13:23 +0900

    feat : change logo

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import LogoImg from "../../public/assets/logo.png";
-import Image from "next/image";
+import LogoImg from '../../public/assets/logo.png';
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
@@ -11,13 +11,11 @@ export default function Navbar() {
       >
         <i className="bi bi-filter-left px-2 bg-gray-900 rounded-md"></i>
       </span>
-      <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900">
+      <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-[#2946A2]">
         <div className="text-gray-100 text-xl">
           <div className="p-2.5 mt-1 flex items-center justify-items-start">
-            <Image src={LogoImg} alt="우리로고" className="h-8 w-auto"></Image>
-            <h1 className="font-bold text-gray-200 text-[15px] ml-3">
-              ㅇㅇ님의 한입노트
-            </h1>
+            <Image src={LogoImg} alt="우리로고" className="h-14 w-auto"></Image>
+            <h1 className="font-bold text-gray-200 text-[15px] ml-3">ㅇㅇ님의 한입노트</h1>
             <i
               className="bi bi-x cursor-pointer ml-28 lg:hidden"
               // onclick="openSidebar()"
@@ -35,9 +33,7 @@ export default function Navbar() {
         </div>
         <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
           <i className="bi bi-house-door-fill"></i>
-          <span className="text-[15px] ml-4 text-gray-200 font-bold">
-            내 뇌
-          </span>
+          <span className="text-[15px] ml-4 text-gray-200 font-bold">내 뇌</span>
         </div>
 
         <div className="my-4 bg-gray-600 h-[1px]"></div>
@@ -49,28 +45,17 @@ export default function Navbar() {
           <i className="bi bi-chat-left-text-fill"></i>
           <div className="flex justify-between w-full items-center">
             {/* 하나의 책 단위 */}
-            <span className="text-[15px] ml-4 text-gray-200 font-bold">
-              MY CS BOOK
-            </span>
+            <span className="text-[15px] ml-4 text-gray-200 font-bold">MY CS BOOK</span>
             <span className="text-sm rotate-180" id="arrow">
               <i className="bi bi-chevron-down"></i>
             </span>
           </div>
         </div>
-        <div
-          className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold"
-          id="submenu"
-        >
+        <div className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu">
           {/* 책 카테고리 */}
-          <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-            운영체제
-          </h1>
-          <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-            네트워크
-          </h1>
-          <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-            자료구조
-          </h1>
+          <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">운영체제</h1>
+          <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">네트워크</h1>
+          <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">자료구조</h1>
         </div>
         <div className="my-4 bg-gray-600 h-[1px]"></div>
         <div
@@ -80,28 +65,17 @@ export default function Navbar() {
           <i className="bi bi-chat-left-text-fill"></i>
           <div className="flex justify-between w-full items-center">
             {/* 하나의 책 단위 */}
-            <span className="text-[15px] ml-4 text-gray-200 font-bold">
-              MY CS BOOK
-            </span>
+            <span className="text-[15px] ml-4 text-gray-200 font-bold">MY CS BOOK</span>
             <span className="text-sm rotate-180" id="arrow">
               <i className="bi bi-chevron-down"></i>
             </span>
           </div>
         </div>
-        <div
-          className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold"
-          id="submenu"
-        >
+        <div className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu">
           {/* 책 카테고리 */}
-          <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-            운영체제
-          </h1>
-          <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-            네트워크
-          </h1>
-          <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-            자료구조
-          </h1>
+          <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">운영체제</h1>
+          <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">네트워크</h1>
+          <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">자료구조</h1>
         </div>
       </div>
     </>
