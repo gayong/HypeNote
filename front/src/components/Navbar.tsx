7811bb1c3b66c1d7cf36fcbabcb261b08c89cce1
이가영 <lgy4625@naver.com>
2023-10-25 15:50:23 +0900

    Feat : sesarch bar

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
           </div>
           {/* <div className="my-2 bg-gray-600 h-[1px]"></div> */}
         </div>
-        <div className="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-white-700 text-white">
+        <div className="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-[#FFFFFF] text-black">
           <i className="bi bi-search text-sm"></i>
           <input
             type="text"
