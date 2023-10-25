054f2a32e0a4f6c87b5e685202dfba54e94cb3e3
simgyuryeol <tlarbfuf@gmail.com>
2023-10-26 00:14:58 +0900

    feat : Response 형식 설정
    ResponseEntity<ApiResponse>로 형식 설정

--- editor/src/main/java/com/surf/editor/controller/EditorController.java
+++ editor/src/main/java/com/surf/editor/controller/EditorController.java
@@ -1,11 +1,15 @@
 package com.surf.editor.controller;
 
+import com.surf.editor.common.response.ApiResponse;
 import com.surf.editor.service.EditorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/editor")
@@ -15,8 +19,16 @@ public class EditorController {
     final private EditorService editorService;
 
     @PostMapping("/{userId}")
-    public void editorCreate(@PathVariable String userId){
+    public ResponseEntity<ApiResponse> editorCreate(@PathVariable String userId){
         editorService.editorCreate(userId);
+
+        ApiResponse apiResponse = ApiResponse.builder()
+                .message("게시판 작성")
+                .status(OK.value())
+                .data(null)
+                .build();
+
+        return ResponseEntity.ok(apiResponse);
     }
 
 }
