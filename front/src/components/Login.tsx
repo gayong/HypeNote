7a688ea5d15b07ef8579d30d1d7100d63b84bb53
윤자현 <bajh323@naver.com>
2023-10-25 17:04:08 +0900

    Style: 로그인 로고 변경

--- front/src/components/Login.tsx
+++ front/src/components/Login.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-import logoImg from "../../public/assets/logo.png";
+import logoImg from "../../public/assets/logo_blue.png";
 import Label from "./ui/Label";
 
 export default function Login() {
