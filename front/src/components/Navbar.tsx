2e7e2cefa910ef82e848dbd42099a3fe777f56f1
이가영 <lgy4625@naver.com>
2023-10-25 15:43:08 +0900

    Feat : search

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -21,9 +21,9 @@ export default function Navbar() {
               // onclick="openSidebar()"
             ></i>
           </div>
-          <div className="my-2 bg-gray-600 h-[1px]"></div>
+          {/* <div className="my-2 bg-gray-600 h-[1px]"></div> */}
         </div>
-        <div className="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-gray-700 text-white">
+        <div className="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-white-700 text-white">
           <i className="bi bi-search text-sm"></i>
           <input
             type="text"
