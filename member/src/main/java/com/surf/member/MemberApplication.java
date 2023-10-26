aa2cfce42e1500b4d928c1faf4d9d4d5892e868d
권인식 <kwon2566@naver.com>
2023-10-26 16:06:59 +0900

    Feat: Member Basic Setting For MSA
    Properties Setting

--- member/src/main/java/com/surf/member/MemberApplication.java
+++ member/src/main/java/com/surf/member/MemberApplication.java
@@ -1,8 +1,10 @@
-package com.member.member;
+package com.surf.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MemberApplication {
 
