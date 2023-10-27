518e5fdf20273b9e72a2a61ad8477e02d38ea4b9
이가영 <lgy4625@naver.com>
2023-10-27 11:20:50 +0900

    Feat : dark
    Change-Id: Ia6362d81b20e3487c69e11c6b37a70055518b413

--- front/src/components/Signin.tsx
+++ front/src/components/Signin.tsx
@@ -8,7 +8,9 @@ import Input from './ui/Input';
 import Label from './ui/Label';
 
 import logoImg from '../../public/assets/logo_blue.png';
+import darkLogoImg from '../../public/assets/logo.png';
 import krLogoImg from '../../public/assets/krlogo_blue.png';
+import darkKrLogoImg from '../../public/assets/krlogo.png';
 
 export default function Signin() {
   const handleSignin = () => {
@@ -18,10 +20,23 @@ export default function Signin() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <Image className="mx-auto h-[20vh] w-auto" src={logoImg} alt="Hype Note 로고" priority />
+          <Image className="mx-auto h-[20vh] w-auto dark:hidden" src={logoImg} alt="일반모드 로고" priority />
+          <Image
+            className="mx-auto h-[20vh] w-auto dark:block hidden"
+            src={darkLogoImg}
+            alt="다크모드일때 로고"
+            priority
+          />
           <h2 className="mt-3 text-center text-2xl leading-9 tracking-tight text-gray-700">
-            로그인 후 <Image className="inline mb-1 h-[1.4rem] w-auto" priority src={krLogoImg} alt="한글로고" />를
-            즐겨보세요 ✏️
+            로그인 후{' '}
+            <Image className="inline mb-2 h-[1.6rem] w-auto dark:hidden" priority src={krLogoImg} alt="한글로고" />
+            <Image
+              className="mb-2 h-[1.6rem] w-auto dark:inline-block hidden"
+              priority
+              src={darkKrLogoImg}
+              alt="다크모드일때 한글로고"
+            />
+            를 즐겨보세요 ✏️
           </h2>
         </div>
 
@@ -53,7 +68,7 @@ export default function Signin() {
               <div className="my-4 bg-line_primary h-[1px] w-full "></div>
             </div>
             <div className="absolute flex justify-center w-full top-1/2 transform -translate-y-1/2">
-              <span className="bg-secondary px-2">또는</span>
+              <span className="bg-secondary dark:bg-dark_background px-2">또는</span>
             </div>
           </div>
 
