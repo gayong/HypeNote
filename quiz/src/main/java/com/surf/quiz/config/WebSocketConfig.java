ee229905ccea9a043d0a12eef67b4a579d0f56f9
최상익 <qjxltm@naver.com>
2023-10-30 15:55:02 +0900

    fix:설정 변경
    Change-Id: I1e7f7a910fb50056c3bc54f8e082097075532fef

--- quiz/src/main/java/com/surf/quiz/config/WebSocketConfig.java
+++ quiz/src/main/java/com/surf/quiz/config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package com.surf.quiz.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,9 +14,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/stomp")  // 서버의 웹소켓 엔드 포인트 등록
-                .setAllowedOriginPatterns("*"); // 모든 오리진 허용
-//                .withSockJS(); // 커넥션을 맺는 경로 설정
+        registry.addEndpoint("/api/stomp/ws")  // 서버의 웹소켓 엔드 포인트 등록
+                .setAllowedOriginPatterns("*") // 모든 오리진 허용
+                .withSockJS(); // 커넥션을 맺는 경로 설정
 //                 테스트 환경에선 주석 처리
 //                .setAllowedOrigins("https://k9e101.p.ssafy.io", "http://k9e101.p.ssafy.io");
 //                만약 WebSocket을 사용할 수 없는 브라우저라면 다른 방식을 사용하도록 설정
