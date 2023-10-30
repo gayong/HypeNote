4811c87211acc1dacad563f9ee3d1650d8fef682
이세울 <swcl309@naver.com>
2023-10-30 18:01:22 +0900

    Feat: editor test

--- front/src/components/editor/Editor.tsx
+++ front/src/components/editor/Editor.tsx
@@ -1,33 +1,66 @@
 "use client";
 
 import { Editor as NovelEditor } from "novel";
-import { useState, useEffect } from "react";
-import { Editor as Editor$1, JSONContent } from "@tiptap/core";
 import styles from "./Editor.module.css";
+import * as Y from "yjs";
+import Collaboration from "@tiptap/extension-collaboration";
+import { HocuspocusProvider } from "@hocuspocus/provider";
+import { useEffect, useState } from "react";
+import { JSONContent } from "@tiptap/core";
+
 type Props = {
   id: string; // id를 문자열로 지정
 };
 
 export default function Editor({ id }: Props) {
-  // editorPage Id
-  // const EditorpageId = id;
-  const [value, setValue] = useState<JSONContent | undefined>({});
-  // const [item, setItem] = useState<JSONContent | undefined>({});
-  // useEffect(() => {
-  //   const local = localStorage.getItem('novel__content');
-  //   if (local) {
-  //     setItem(JSON.parse(local));
-  //   }
-  // }, [value]);
+  const [value, setValue] = useState<JSONContent>({});
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
 
+  // 소켓 연결
+  // const provider = new HocuspocusProvider({
+  //   url: "ws://127.0.0.1:1234",
+  //   name: "example-document",
+  // });
+
+  // document 연결
+  // const ydoc = new Y.Doc();
+  // console.log("dlfsdf");
+  // const extensions = Collaboration.configure({
+  //   document: provider.document,
+  // });
+  const data = {
+    type: "doc",
+    content: [
+      {
+        type: "paragraph",
+        content: [{ type: "text", text: "adsfsdf" }],
+      },
+      {
+        type: "paragraph",
+        content: [{ type: "text", text: "asdfsdf" }],
+      },
+      {
+        type: "paragraph",
+        content: [{ type: "text", text: "asdfadsfasdfasd" }],
+      },
+      {
+        type: "heading",
+        content: [{ type: "text", text: "심규룔" }],
+        attrs: { level: 1 },
+      },
+    ],
+  };
   return (
     <>
       <NovelEditor
-        defaultValue={{}}
+        defaultValue={data}
         onUpdate={(editor) => setValue(editor?.getJSON())}
         className={styles["editor-container"]}
+        // extensions={[extensions]}
+        disableLocalStorage={true}
       />
-      {/* <NovelEditor defaultValue={{}} disableLocalStorage={true} value={item}/> */}
     </>
   );
 }
