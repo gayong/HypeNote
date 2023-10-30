9a755689c0132b4520a9831d78371f25a50b4484
최상익 <qjxltm@naver.com>
2023-10-30 14:46:41 +0900

    fix:웹소켓 경로 수정
    Change-Id: I6416ab0e32660b5c0a21a7d01e2abf578005ae43

--- quiz/src/main/java/com/surf/quiz/config/WebSocketConfig.java
+++ quiz/src/main/java/com/surf/quiz/config/WebSocketConfig.java
@@ -14,8 +14,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/api/stomp")  // 서버의 웹소켓 엔드 포인트 등록
-                .setAllowedOriginPatterns("*"); // 모든 오리진 허용
-//                .withSockJS(); // 커넥션을 맺는 경로 설정
+                .setAllowedOriginPatterns("*") // 모든 오리진 허용
+                .withSockJS(); // 커넥션을 맺는 경로 설정
 //                 테스트 환경에선 주석 처리
 //                .setAllowedOrigins("https://k9e101.p.ssafy.io", "http://k9e101.p.ssafy.io");
 //                만약 WebSocket을 사용할 수 없는 브라우저라면 다른 방식을 사용하도록 설정
