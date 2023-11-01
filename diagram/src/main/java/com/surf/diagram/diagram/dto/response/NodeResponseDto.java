2dd0fa60f211a6db2bf9a099964af19474f0b6d0
최상익 <qjxltm@naver.com>
2023-11-01 16:58:50 +0900

    Fix: 노드 링크 함수 수정
    Change-Id: I74563d8430708d020376601e1ba348b102dfe47b

--- diagram/src/main/java/com/surf/diagram/diagram/dto/response/NodeResponseDto.java
+++ diagram/src/main/java/com/surf/diagram/diagram/dto/response/NodeResponseDto.java
@@ -2,19 +2,15 @@ package com.surf.diagram.diagram.dto.response;
 
 
 import com.surf.diagram.diagram.entity.Link;
-import com.surf.diagram.diagram.entity.Node;
-import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
-@Data
-@NoArgsConstructor
 @Getter
 @Setter
 public class NodeResponseDto {
-    private List<Node> nodes;
+    private List<NodeDto> nodes;
     private List<Link> links;
+
 }
