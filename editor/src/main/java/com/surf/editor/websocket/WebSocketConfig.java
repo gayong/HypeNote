7cb7e78fa4ce1247855838fe1cc2b5737745f833
simgyuryeol <wvxyz@naver.com>
2023-11-01 23:39:03 +0900

    feat : withSockJS 주석 해제, endpoint 이름 변경

--- editor/src/main/java/com/surf/editor/websocket/WebSocketConfig.java
+++ editor/src/main/java/com/surf/editor/websocket/WebSocketConfig.java
@@ -26,8 +26,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     // 클라이언트에서 WebSocket에 접속할 수 있는 endpoint를 지정한다.
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/api/editor/stomp") // ex) ws://localhost:8080/api/editor/stomp
-                .setAllowedOriginPatterns("*");
-//                .withSockJS(); //테스트할때는 주석처리, 테스트할때 ws대신 http사용
+        registry.addEndpoint("/api/editor/ws") // ex) ws://localhost:8080/api/editor/stomp
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); //테스트할때는 주석처리, 테스트할때 ws대신 http사용
     }
 }
