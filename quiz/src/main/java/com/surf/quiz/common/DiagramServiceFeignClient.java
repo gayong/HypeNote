cd4c7ee36d3c67bc0f7b0a5d976ab3150a8c38f8
최상익 <qjxltm@naver.com>
2023-11-03 21:37:55 +0900

    fix:fegin 설정
    Change-Id: Icfa6012b1af1e0ccbfddeabd8ab6c06d22369995

--- quiz/src/main/java/com/surf/quiz/common/DiagramServiceFeignClient.java
+++ quiz/src/main/java/com/surf/quiz/common/DiagramServiceFeignClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name="server-diagram", url="https://k9e101.p.ssafy.io")
+//@FeignClient(name="server-diagram", url="http://localhost:8087")
 public interface DiagramServiceFeignClient {
     @GetMapping("/api/diagram/{userId}")
     BaseResponse<DiagramResponseDto> getNodes(@PathVariable int userId);
