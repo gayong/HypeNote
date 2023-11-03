04f526d2b040cb4ad5f31148558d070191577784
윤자현 <bajh323@naver.com>
2023-11-03 16:18:07 +0900

    Fix: 룸리스트 소켓 연결 끊기는 것 해결

--- front/src/app/quiz/room/page.tsx
+++ front/src/app/quiz/room/page.tsx
@@ -14,7 +14,7 @@ export default function QuizRoomPage() {
   return (
     <>
       {/* <button className="bg-primary flex mx-auto hover:bg-gray-300 text-secondary py-2 px-4 rounded">방 만들기</button> */}
-      <section className="flex items-center justify-center h-screen">
+      <section className="flex items-center justify-center ">
         <QuizList />
       </section>
     </>
