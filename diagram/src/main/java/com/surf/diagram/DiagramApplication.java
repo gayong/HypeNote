42b70e72c01ed2b5c680f844bfe137032f4b38c2
최상익 <qjxltm@naver.com>
2023-11-04 20:19:42 +0900

    Fix:test
    Change-Id: If0b5a168675440d8582eaab775b7fa5e72c15d30

--- diagram/src/main/java/com/surf/diagram/DiagramApplication.java
+++ diagram/src/main/java/com/surf/diagram/DiagramApplication.java
@@ -2,7 +2,9 @@ package com.surf.diagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class DiagramApplication {
 
