20a617bf8570c6f7182dc088f35f858f89736281
최상익 <qjxltm@naver.com>
2023-11-06 02:32:23 +0900

    Fix: komoran
    Change-Id: I8d7a43777eda1d2f7cffa99c0e8a784d8614e041

--- diagram/src/main/java/com/surf/diagram/diagram/dto/response/LinkResponseDto.java
+++ diagram/src/main/java/com/surf/diagram/diagram/dto/response/LinkResponseDto.java
@@ -1,11 +1,13 @@
 package com.surf.diagram.diagram.dto.response;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class LinkResponseDto {
     private int source;
     private int target;
--- diagram/src/main/java/com/surf/diagram/diagram/dto/response/NodeResponseDto.java
+++ diagram/src/main/java/com/surf/diagram/diagram/dto/response/NodeResponseDto.java
@@ -1,11 +1,13 @@
 package com.surf.diagram.diagram.dto.response;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class NodeResponseDto {
     private Long id;
     private String title;
