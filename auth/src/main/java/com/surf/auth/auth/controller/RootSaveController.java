92103e7d76c8f94714ecc1cdd892cf2bc5d727de
권인식 <kwon2566@naver.com>
2023-11-07 17:58:45 +0900

    Feat: Log for Root Save(Feign)
    for GyuRyeol

--- auth/src/main/java/com/surf/auth/auth/controller/RootSaveController.java
+++ auth/src/main/java/com/surf/auth/auth/controller/RootSaveController.java
@@ -4,10 +4,12 @@ package com.surf.auth.auth.controller;
 import com.surf.auth.auth.dto.RootDto;
 import com.surf.auth.auth.service.RootSaveService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @Controller
 @RequiredArgsConstructor
@@ -18,6 +20,9 @@ public class RootSaveController {
 
     @PutMapping("/root-save")
     public HttpStatus LogIn(@RequestBody RootDto rootDto) {
+        
+        log.info("컨트롤러");
+
         return rootSaveService.rootSaveService(rootDto);
     }
 }
