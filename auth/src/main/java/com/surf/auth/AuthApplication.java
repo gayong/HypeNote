22169d970286ee67b805a8bb0694f3a6fad86576
권인식 <kwon2566@naver.com>
2023-11-07 16:32:05 +0900

    Fix: Dependency & Properties Change for MSA

--- auth/src/main/java/com/surf/auth/AuthApplication.java
+++ auth/src/main/java/com/surf/auth/AuthApplication.java
@@ -2,8 +2,11 @@ package com.surf.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+
+@EnableDiscoveryClient
 @EnableWebSecurity
 @SpringBootApplication
 public class AuthApplication {
