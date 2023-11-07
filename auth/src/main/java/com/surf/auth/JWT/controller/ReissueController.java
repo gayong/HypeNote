8bb9b909410a3b479d63dd3fff306d5b52aaeee2
권인식 <kwon2566@naver.com>
2023-11-08 04:42:51 +0900

    Feat: Access Token Reissue Logic(not Complete)
    ReissueController Success
    refreshTokenAuthentication Success
    reissueAccessToken Sucess
    findRefreshTokenByUserId Success

--- auth/src/main/java/com/surf/auth/JWT/controller/ReissueController.java
+++ auth/src/main/java/com/surf/auth/JWT/controller/ReissueController.java
@@ -1,8 +1,13 @@
 package com.surf.auth.JWT.controller;
 
 import com.surf.auth.JWT.service.ReissueService;
+import com.surf.auth.auth.dto.TokenDto;
+import com.surf.auth.auth.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +19,30 @@ public class ReissueController {
     private final ReissueService reissueService;
 
     @PostMapping("/reissue")
-    public void reissue() {
-        reissueService.reissueService();
+    public ResponseEntity<TokenDto> reissue(@RequestBody String refreshToken) {
+
+        User userInfo = reissueService.parsingRefreshToken(refreshToken);
+
+        String storedRefreshToken = reissueService.findFreshTokenByUserId(String.valueOf(userInfo.getUserId()));
+
+        TokenDto fail = new TokenDto();
+
+        if ( storedRefreshToken != null) {
+            boolean authenticationResult = reissueService.refreshTokenAuthentication(refreshToken, storedRefreshToken);
+            if (authenticationResult) {
+
+                return ResponseEntity.ok(reissueService.reissueAccessToken(userInfo));
+            } else {
+
+                fail.setMessage("유효하지 않은 토큰입니다.");
+
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(fail);
+            }
+        } else {
+
+            fail.setMessage("해당 유저가 가진 RefreshToken이 없습니다.");
+
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(fail);
+        }
     }
 }
--- auth/src/main/java/com/surf/auth/JWT/service/ReissueService.java
+++ auth/src/main/java/com/surf/auth/JWT/service/ReissueService.java
@@ -1,11 +1,52 @@
 package com.surf.auth.JWT.service;
 
+import com.surf.auth.auth.dto.TokenDto;
+import com.surf.auth.auth.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import java.util.Objects;
 
+@RequiredArgsConstructor
 @Service
 public class ReissueService {
 
-    public void reissueService() {
+    private final StringRedisTemplate redisTemplate;
+    private final AccessTokenIssueService accessTokenIssueService;
 
+    @Value("${jwt.secret}")
+    private String SECRET;
+    public User parsingRefreshToken(String refreshToken) {
+
+        User userInfo = new User();
+
+        userInfo.setUserId();
+        userInfo.setEmail();
+        userInfo.setNickName();
+        userInfo.setProfileImage();
+
+        return userInfo;
+    }
+
+    public String findFreshTokenByUserId(String userId) {
+
+        return redisTemplate.opsForValue().get(userId);
+    }
+
+    public boolean refreshTokenAuthentication(String refreshToken, String storedRefreshToken) {
+        return Objects.equals(refreshToken, storedRefreshToken);
+    }
+
+    public TokenDto reissueAccessToken(User userInfo) {
+
+        TokenDto tokenIssueResult = new TokenDto();
+
+        tokenIssueResult.setAccessToken(accessTokenIssueService.accessTokenIssue(userInfo));
+        tokenIssueResult.setMessage("Access Token이 정상 발급 되었습니다.");
+
+        return tokenIssueResult;
     }
 }
