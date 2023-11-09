6cb593e1eb4c44b490061564f32d167324cd3fbb
simgyuryeol <wvxyz@naver.com>
2023-11-09 15:34:56 +0900

    Feat : 로컬 실행 test 중

--- search/src/main/java/com/surf/search/controller/SearchController.java
+++ search/src/main/java/com/surf/search/controller/SearchController.java
@@ -4,6 +4,7 @@ import com.surf.search.common.response.ApiResponse;
 import com.surf.search.dto.SearchGetResponseDto;
 import com.surf.search.service.SearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/search")
+@RequiredArgsConstructor
 public class SearchController {
 
     private final SearchService searchService;
