1039467d9349ee5e8212b8b87907f43c84828ee1
권인식 <kwon2566@naver.com>
2023-11-10 12:21:37 +0900

    Feat: UserInformation Controller - user Information NickName Controller GET Complete

--- auth/src/main/java/com/surf/auth/member/controller/UserInformationController.java
+++ auth/src/main/java/com/surf/auth/member/controller/UserInformationController.java
@@ -3,12 +3,16 @@ package com.surf.auth.member.controller;
 
 import com.surf.auth.JWT.decoder.TokenDecoder;
 import com.surf.auth.member.authenticator.UserEmailAuthenticator;
+import com.surf.auth.member.authenticator.UserNickNameAuthenticator;
 import com.surf.auth.member.dto.UserInfoResponseDto;
 import com.surf.auth.member.service.UserInformationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,19 +21,38 @@ public class UserInformationController {
 
     private final UserInformationService userInformationService;
     private final UserEmailAuthenticator userEmailAuthenticator;
+    private final UserNickNameAuthenticator userNickNameAuthenticator;
     private final TokenDecoder tokenDecoder;
 
     @PostMapping("/user-info")
     private ResponseEntity<UserInfoResponseDto> userInformationController (@RequestHeader String accessToken) {
 
-        String email = tokenDecoder.parsingAccessToken(accessToken);
+        Map<String, String> request = new HashMap<>();
 
-        if (userEmailAuthenticator.userEmailAuthenticator(email)) {
+        request.put("email", tokenDecoder.parsingAccessToken(accessToken));
 
-            return ResponseEntity.ok(userInformationService.sendUserInformation(email));
+        if (userEmailAuthenticator.userEmailAuthenticator(request.get("email"))) {
+
+            return ResponseEntity.ok(userInformationService.sendUserInformation(request));
 
         } else {
 
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(userInformationService.userNotFound());
+        }
+    }
+
+    @GetMapping("/user-info/{nickName}")
+    private ResponseEntity<UserInfoResponseDto> userInformationNickNameController (@PathVariable String nickName) {
+
+        Map<String, String> request = new HashMap<>();
+
+        request.put("nickName", nickName);
+
+        if (userNickNameAuthenticator.userNickNameAuthentication(nickName)) {
+
+            return ResponseEntity.ok(userInformationService.sendUserInformation(request));
+        }
+        else {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(userInformationService.userNotFound());
         }
     }
