94ca7ad173f8773a60d7746811545ce09d36e756
simgyuryeol <wvxyz@naver.com>
2023-11-10 17:34:57 +0900

    Feat : kakao web search 개발

---
+++ search/src/main/java/com/surf/search/kakao/dto/KakaoSearchRequestDto.java
@@ -0,0 +1,25 @@
+package com.surf.search.kakao.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class KakaoSearchRequestDto {
+
+    private List<Items> documents;
+
+    @Getter
+    @Setter
+    public static class Items{
+        private String title;
+        private String contents;
+        private String url;
+        private String blogname;
+        private String thumbnail;
+        private String datetime;
+    }
+
+}
---
+++ search/src/main/java/com/surf/search/kakao/service/KakaoSearchService.java
@@ -0,0 +1,77 @@
+package com.surf.search.kakao.service;
+
+import com.surf.search.kakao.dto.KakaoSearchRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoSearchService {
+
+    @Value("${kakao.search.key}")
+    private String key;
+    private static String url = "https://dapi.kakao.com/v2/search/blog";
+
+    public KakaoSearchRequestDto kakaoSearchGet(String query) {
+//        try{
+            WebClient webClient = WebClient.builder()
+                    .baseUrl(url)
+                    .defaultHeader("Authorization","KakaoAK "+key)
+                    .build();
+
+            KakaoSearchRequestDto webClientResponse = webClient.get()
+                    .uri(uriBuilder -> uriBuilder.path("")
+                            .queryParam("query",query)
+                            .build()
+                    )
+                    .retrieve()
+                    .bodyToMono(KakaoSearchRequestDto.class)
+                    .block();
+
+        // title에 대해 HTML 태그 제거
+        if (webClientResponse != null && webClientResponse.getDocuments() != null) {
+            for (KakaoSearchRequestDto.Items item : webClientResponse.getDocuments()) {
+                if (item.getTitle() != null) {
+                    item.setTitle(item.getTitle().replaceAll("(&lt;(/)?([a-zA-Z]*)(\\s[a-zA-Z]*=[^>]*)?(\\s)*(/)?&gt;)|(<(/)?([a-zA-Z]*)(\\s[a-zA-Z]*=[^>]*)?(\\s)*(/)?>)", ""));
+                    item.setTitle(removeHtmlEntities(item.getTitle()));
+                }
+
+                // contents에 대해 HTML 태그 제거
+                if (item.getContents() != null) {
+                    item.setContents(item.getContents().replaceAll("(&lt;(/)?([a-zA-Z]*)(\\s[a-zA-Z]*=[^>]*)?(\\s)*(/)?&gt;)|(<(/)?([a-zA-Z]*)(\\s[a-zA-Z]*=[^>]*)?(\\s)*(/)?>)", ""));
+                    item.setContents(removeHtmlEntities(item.getContents()));
+                }
+            }
+        }
+
+            return webClientResponse;
+
+//        }catch (Exception e){
+//            throw new BaseException(ErrorCode.ILLEGAL_ARGUMENT_EXCEPTION);
+//        }
+    }
+
+    public static String removeHtmlEntities(String input) {
+        // HTML 엔터티를 찾을 정규식
+        String regex = "&#[0-9]+;|&[a-zA-Z]+;";
+
+        // 정규식을 사용하여 매치된 엔터티 제거
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        StringBuffer buffer = new StringBuffer();
+
+        while (matcher.find()) {
+            matcher.appendReplacement(buffer, "");
+        }
+        matcher.appendTail(buffer);
+
+        return buffer.toString();
+    }
+
+
+}
