b0cd36b88f93993cc8f744afadfe3e9e8beeb558
simgyuryeol <wvxyz@naver.com>
2023-11-11 23:27:43 +0900

    Fix : FeignClient name 변경

--- editor/src/main/java/com/surf/editor/feign/client/MemberUnShareOpenFeign.java
+++ editor/src/main/java/com/surf/editor/feign/client/MemberUnShareOpenFeign.java
@@ -4,7 +4,7 @@ import com.surf.editor.feign.dto.MemberShareRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(name = "MemberShareClient",url = "https://www.hype-note.com/api/auth")
+@FeignClient(name = "MemberUnShareClient",url = "https://www.hype-note.com/api/auth")
 public interface MemberUnShareOpenFeign {
     @PutMapping(value = "/unshare") // 수정 필요
     void MemberUnShare(MemberShareRequestDto memberShareRequestDto);
