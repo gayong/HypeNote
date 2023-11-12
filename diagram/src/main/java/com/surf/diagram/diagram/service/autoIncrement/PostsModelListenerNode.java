bb1dd9f8dfaae11e9f1f57e3ff17c149fce120a8
최상익 <qjxltm@naver.com>
2023-11-13 01:36:19 +0900

    Fix: id 삭제
    Change-Id: I13c518b34c6431df8fca2aeadf52d4b49ad0a6cc

--- diagram/src/main/java/com/surf/diagram/diagram/service/autoIncrement/PostsModelListenerNode.java
+++ diagram/src/main/java/com/surf/diagram/diagram/service/autoIncrement/PostsModelListenerNode.java
@@ -1,21 +1,21 @@
-package com.surf.diagram.diagram.service.autoIncrement;
-
-import com.surf.diagram.diagram.entity.Node;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
-import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
-import org.springframework.stereotype.Component;
-
-@RequiredArgsConstructor
-@Component
-public class PostsModelListenerNode extends AbstractMongoEventListener<Node> {
-    private final SequenceGeneratorService sequenceGenerator;
-
-    @Override
-    public void onBeforeConvert(BeforeConvertEvent<Node> event) {
-        if (event.getSource().getId() == null) {
-            event.getSource().setId(sequenceGenerator.generateSequence(Node.SEQUENCE_NAME));
-        }
-    }
-
-}
+//package com.surf.diagram.diagram.service.autoIncrement;
+//
+//import com.surf.diagram.diagram.entity.Node;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+//import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
+//import org.springframework.stereotype.Component;
+//
+//@RequiredArgsConstructor
+//@Component
+//public class PostsModelListenerNode extends AbstractMongoEventListener<Node> {
+//    private final SequenceGeneratorService sequenceGenerator;
+//
+//    @Override
+//    public void onBeforeConvert(BeforeConvertEvent<Node> event) {
+//        if (event.getSource().getId() == null) {
+//            event.getSource().setId(sequenceGenerator.generateSequence(Node.SEQUENCE_NAME));
+//        }
+//    }
+//
+//}
