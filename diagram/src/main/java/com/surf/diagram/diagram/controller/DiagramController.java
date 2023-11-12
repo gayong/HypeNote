516b9b86c93acc8b401bd72203ee844bd7cfa033
최상익 <qjxltm@naver.com>
2023-11-13 01:34:53 +0900

    Fix: 컨트롤러 수정
    Change-Id: Ica6f457e366c09a25a8310796fc3a6dde8c2d82e

--- diagram/src/main/java/com/surf/diagram/diagram/controller/DiagramController.java
+++ diagram/src/main/java/com/surf/diagram/diagram/controller/DiagramController.java
@@ -1,6 +1,9 @@
 package com.surf.diagram.diagram.controller;
 
 import com.surf.diagram.diagram.common.BaseResponse;
+import com.surf.diagram.diagram.dto.editor.EditorListRequestDto;
+import com.surf.diagram.diagram.dto.editor.EditorListResponseDto;
+import com.surf.diagram.diagram.dto.member.UserInfoResponseDto;
 import com.surf.diagram.diagram.dto.response.LinkResponseDto;
 import com.surf.diagram.diagram.dto.response.NodeResponseDto;
 import com.surf.diagram.diagram.dto.response.DiagramResponseDto;
@@ -9,6 +12,8 @@ import com.surf.diagram.diagram.entity.Node;
 import com.surf.diagram.diagram.repository.LinkRepository;
 import com.surf.diagram.diagram.repository.NodeRepository;
 import com.surf.diagram.diagram.service.DiagramService;
+import com.surf.diagram.diagram.service.FeginEditorService;
+import com.surf.diagram.diagram.service.FeginUserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,62 +29,57 @@ import java.util.stream.Collectors;
 public class DiagramController {
 
     private final DiagramService diagramService;
+    private final FeginEditorService feginEditorService;
+    private final FeginUserService feginUserService;
     private final NodeRepository nodeRepository;
     private final LinkRepository linkRepository;
 
-    public DiagramController(DiagramService diagramService, NodeRepository nodeRepository ,LinkRepository linkRepository) {
+    public DiagramController(DiagramService diagramService, NodeRepository nodeRepository ,LinkRepository linkRepository, FeginEditorService feginEditorService, FeginUserService feginUserService) {
         this.diagramService = diagramService;
         this.nodeRepository = nodeRepository;
         this.linkRepository = linkRepository;
+        this.feginEditorService = feginEditorService;
+        this.feginUserService = feginUserService;
     }
 
     @GetMapping("/{userId}")
     @Operation(summary = "내 노드와 링크 조회")
-    public BaseResponse<DiagramResponseDto> getNodes(@PathVariable int userId, HttpServletRequest request) {
-        String token = request.getHeader("Authorization");
-        List<Node> nodes = nodeRepository.findByUserId(userId);
-        List<Link> links = linkRepository.findByUserId(userId);
+    public BaseResponse<DiagramResponseDto> getNodes(@PathVariable int userId) {
+        System.out.println("userId = " + userId);
 
-        List<NodeResponseDto> nodeResponseDtos = nodes.stream()
-                .map(node -> new NodeResponseDto(node.getId(), node.getTitle(), node.getUserId(), node.getEditorId()))
-                .collect(Collectors.toList());
+        DiagramResponseDto response = diagramService.getDiagram(userId);
 
-        List<LinkResponseDto> linkResponseDtos = links.stream()
-                .map(link -> new LinkResponseDto(link.getSource(), link.getTarget(),  Math.exp(link.getSimilarity()) * 1, link.getUserId()))
-                .collect(Collectors.toList());
-
-        DiagramResponseDto response = new DiagramResponseDto(nodeResponseDtos, linkResponseDtos);
         return new BaseResponse<>(response);
     }
 
 
 
-    @PostMapping("/keyword/{userId}")
-    @Operation(summary = "키워드 분석")
-    public BaseResponse<String> classifyAndSaveEmptyCategoryDiagrams(@PathVariable int userId) throws Exception {
-        diagramService.classifyAndSaveEmptyCategoryNodes(userId);
-        return new BaseResponse<>("모든 Diagram들이 성공적으로 분석되고 업데이트되었습니다.");
-    }
+//    @PostMapping("/keyword/{userId}")
+//    @Operation(summary = "키워드 분석")
+//    public BaseResponse<String> classifyAndSaveEmptyCategoryDiagrams(@PathVariable int userId) throws Exception {
+//        diagramService.classifyAndSaveEmptyCategoryNodes(userId);
+//        return new BaseResponse<>("모든 Diagram들이 성공적으로 분석되고 업데이트되었습니다.");
+//    }
 
-    @PostMapping("/link/{userId}")
-    @Operation(summary = "링크 연결")
-    public BaseResponse<String> linkNodesByCategoryAndConfidence(@PathVariable int userId) throws Exception {
-        diagramService.linkNodesByCategoryAndConfidence(userId);
-        return new BaseResponse<>("링크 생성 완료");
-    }
+//    @PostMapping("/link/{userId}")
+//    @Operation(summary = "링크 연결")
+//    public BaseResponse<String> linkNodesByCategoryAndConfidence(@PathVariable int userId) throws Exception {
+//        diagramService.linkNodesByCategoryAndConfidence(userId);
+//        return new BaseResponse<>("링크 생성 완료");
+//    }
 
 
-    @PostMapping("/share/{userId}/{targetUserId}")
-    @Operation(summary = "1인 쉐어")
-    public BaseResponse<DiagramResponseDto> linkNodesByShare(@PathVariable int userId, @PathVariable int targetUserId) throws Exception {
-        DiagramResponseDto responseDto = diagramService.linkNodesByShare(userId, targetUserId);
-        return new BaseResponse<>(responseDto);
-    }
+//    @PostMapping("/share/{userId}/{targetUserId}")
+//    @Operation(summary = "1인 쉐어")
+//    public BaseResponse<DiagramResponseDto> linkNodesByShare(@PathVariable int userId, @PathVariable int targetUserId) throws Exception {
+//        DiagramResponseDto responseDto = diagramService.linkNodesByShare(userId, targetUserId);
+//        return new BaseResponse<>(responseDto);
+//    }
 
-    @PostMapping("/share/{userId}")
-    @Operation(summary = "다인 쉐어")
-    public BaseResponse<DiagramResponseDto> linkNodesByShares(@PathVariable int userId, @RequestBody List<Integer> targetIds ) throws Exception {
-        DiagramResponseDto responseDto = diagramService.linkNodesByShares(userId, targetIds);
-        return new BaseResponse<>(responseDto);
-    }
+//    @PostMapping("/share/{userId}")
+//    @Operation(summary = "다인 쉐어")
+//    public BaseResponse<DiagramResponseDto> linkNodesByShares(@PathVariable int userId, @RequestBody List<Integer> targetIds ) throws Exception {
+//        DiagramResponseDto responseDto = diagramService.linkNodesByShares(userId, targetIds);
+//        return new BaseResponse<>(responseDto);
+//    }
 }
