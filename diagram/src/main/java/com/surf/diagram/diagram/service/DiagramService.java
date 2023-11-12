c8fe0b6fc8cddcecac292d5cd6a876f7d481c1c2
최상익 <qjxltm@naver.com>
2023-11-13 01:36:41 +0900

    Fix: diagram 함수 수정
    Change-Id: Ibb44896bdce6733314f3ab2fec8ad88f57807399

--- diagram/src/main/java/com/surf/diagram/diagram/service/DiagramService.java
+++ diagram/src/main/java/com/surf/diagram/diagram/service/DiagramService.java
@@ -1,13 +1,17 @@
 package com.surf.diagram.diagram.service;
 
+import com.surf.diagram.diagram.dto.editor.EditorListResponseDto;
 import com.surf.diagram.diagram.dto.response.DiagramResponseDto;
+import com.surf.diagram.diagram.dto.response.LinkResponseDto;
+import com.surf.diagram.diagram.dto.response.NodeResponseDto;
+import com.surf.diagram.diagram.entity.Node;
 
 import java.util.List;
 
 public interface DiagramService {
-    void classifyAndSaveEmptyCategoryNodes(int userId) throws Exception;
-    void linkNodesByCategoryAndConfidence(int userId);
-    DiagramResponseDto linkNodesByShare(int userId, int targetUserId);
 
-    DiagramResponseDto linkNodesByShares(int userId, List<Integer> targetUserIds);
+    //    DiagramResponseDto linkNodesByShare(int userId, int targetUserId);
+//
+//    DiagramResponseDto linkNodesByShares(int userId, List<Integer> targetUserIds);
+    DiagramResponseDto getDiagram(int userId);
 }
