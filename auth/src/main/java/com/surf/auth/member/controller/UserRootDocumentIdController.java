53b655babd090b6bb4353d02a5d172eba71a9f4c
권인식 <kwon2566@naver.com>
2023-11-13 03:56:48 +0900

    Feat: Root User Info Controller 1st
    rootInspect Generate into Feign Client Interface

--- auth/src/main/java/com/surf/auth/member/controller/UserRootDocumentIdController.java
+++ auth/src/main/java/com/surf/auth/member/controller/UserRootDocumentIdController.java
@@ -1,15 +1,25 @@
 package com.surf.auth.member.controller;
 
 
+import com.surf.auth.member.authenticator.UserPkAuthenticator;
 import com.surf.auth.member.dto.request.RootDto;
 import com.surf.auth.member.dto.request.RootsDto;
+import com.surf.auth.member.dto.request.SharedDocumentsListDto;
+import com.surf.auth.member.dto.response.UserInfoResponseDto;
 import com.surf.auth.member.feign.FeignClientInterface;
+import com.surf.auth.member.service.UserInformationService;
 import com.surf.auth.member.service.UserRootDocumentIdService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -20,6 +30,10 @@ public class UserRootDocumentIdController {
 
     private final UserRootDocumentIdService userRootDocumentIdService;
     private final FeignClientInterface feignClientInterface;
+
+    private final UserInformationService userInformationService;
+
+    private final UserPkAuthenticator userPkAuthenticator;
 
     @PutMapping("/root-save")
     public HttpStatus rootSaveController(@RequestBody RootDto rootDto) {
@@ -34,5 +48,26 @@ public class UserRootDocumentIdController {
 
         feignClientInterface.rootDelete(rootsDto);
         return userRootDocumentIdService.rootDeleteService(rootsDto);
+    }
+
+    @GetMapping("/root-user-info/{userPk}")
+    public ResponseEntity<List<UserInfoResponseDto>> rootUserInfoController(@PathVariable int userPk) {
+
+        List<String> sharedDocumentsList = userInformationService.sendUserInformationPk(userPk).getSharedDocumentsRoots();
+        SharedDocumentsListDto sharedDocumentsListDto = new SharedDocumentsListDto();
+        sharedDocumentsListDto.setSharedDocumentsList(sharedDocumentsList);
+
+        List<Integer> userList = feignClientInterface.rootInspect(sharedDocumentsListDto);
+
+        List<UserInfoResponseDto> userInfoList = new ArrayList<>();
+
+        for (int sharedUserPk : userList) {
+            if (userPkAuthenticator.userPkAuthentication(sharedUserPk)) {
+                userInfoList.add(userInformationService.sendUserInformationPk(sharedUserPk));
+            }
+        }
+        return ResponseEntity.ok(userInfoList);
+
+
     }
 }
--- auth/src/main/java/com/surf/auth/member/feign/FeignClientInterface.java
+++ auth/src/main/java/com/surf/auth/member/feign/FeignClientInterface.java
@@ -1,15 +1,21 @@
 package com.surf.auth.member.feign;
 
 import com.surf.auth.member.dto.request.RootsDto;
+import com.surf.auth.member.dto.request.SharedDocumentsListDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name="chat", url="https://api.openai.com/v1/")
+import java.util.List;
 
+@FeignClient(name="chat", url="https://www.hype-note.com")
 public interface FeignClientInterface {
 
     @DeleteMapping("/api/editor/root-delete")
     HttpStatus rootDelete(@RequestBody RootsDto rootsDto);
+
+    @PostMapping("/api/editor/root-owner")
+    List<Integer> rootInspect(@RequestBody SharedDocumentsListDto sharedDocumentsListDto);
 }
