88ec3a6ef4f5f04b9f62a15295fc999d8fa8320b
윤자현 <bajh323@naver.com>
2023-11-14 00:56:50 +0900

    Feat: signin 연결 해제

--- front/src/api/instances/api.ts
+++ front/src/api/instances/api.ts
@@ -76,7 +76,7 @@ api.interceptors.response.use(
           } catch (error) {
             console.log("실패");
 
-            return (window.location.href = `signin`);
+            // return (window.location.href = `signin`);
           }
         }
         return Promise.reject(error);
