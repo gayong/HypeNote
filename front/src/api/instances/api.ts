fe97bff69eba4465d75b408e476284d7ee55e443
윤자현 <bajh323@naver.com>
2023-11-14 02:15:17 +0900

    Feat: 토큰 수정

--- front/src/api/instances/api.ts
+++ front/src/api/instances/api.ts
@@ -58,8 +58,8 @@ api.interceptors.response.use(
         // if (error.response.status === 401 || error.response.status == 403) {
         if (error.response.status === 401) {
           try {
-            // const res = await axios.get(`${window.location.origin}/api/auth/reissue`, {
-            const res = await api.get(`auth/reissue`, {
+            const res = await axios.get(`${window.location.origin}/api/auth/reissue`, {
+              // const res = await api.get(`auth/reissue`, {
               withCredentials: true,
             });
             localStorage.setItem("accessToken", res.data.accessToken);
