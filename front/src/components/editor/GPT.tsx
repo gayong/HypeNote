fbf81b732d39fa6a350fa6a762885f54c18e0031
이가영 <lgy4625@naver.com>
2023-11-13 15:06:17 +0900

    Feat : gpt 테스트2
    Change-Id: I3eee01e9359a65a10c001abadcaff229fe1496d0

--- front/src/components/editor/GPT.tsx
+++ front/src/components/editor/GPT.tsx
@@ -7,160 +7,116 @@ import { SearchType } from "@/types/ediotr";
 import "../../app/search/search.css";
 import { BsFillSendFill } from "react-icons/bs";
 
-async function fetchEventSource(url: string, keyword: string) {
-  // const controller = new AbortController();
-  let data = "";
+// async function fetchEventSource(url: string, keyword: string) {
+//   let data = "";
 
-  if (keyword) {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ question: keyword }),
-    });
+//   if (keyword) {
+//     const response = await fetch(url, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ question: keyword }),
+//     });
 
-    const contentType = response.headers.get("Content-Type");
-    if (response.body && contentType && contentType.includes("application/json")) {
-      const reader = response.body.getReader();
-      const decoder = new TextDecoder();
+//     const contentType = response.headers.get("Content-Type");
+//     if (response.body && contentType && contentType.includes("application/json")) {
+//       const reader = response.body.getReader();
+//       const decoder = new TextDecoder();
 
-      while (true) {
-        const { value, done } = await reader.read();
-        if (done) break;
+//       while (true) {
+//         const { value, done } = await reader.read();
+//         if (done) break;
 
-        let chunk = decoder.decode(value || new Uint8Array(), { stream: !done });
-        if (chunk.startsWith("data:")) {
-          chunk = chunk.slice(5);
-        }
-        // const jsonData = chunk.slice(6);
-        const result = JSON.parse(chunk);
-
-        if (result && result.choices && result.choices[0]) {
-          data += result.choices[0].delta.content;
-          console.log("dd", data);
-          console.log("result", result);
-          console.log("chunk", chunk);
-        } else if (contentType && contentType.includes("text/html")) {
-          console.log("json 타입이 아님 html임 stream 하지마셈");
-        } else {
-          throw new Error(`Unexpected content type: ${contentType}`);
-        }
-      }
-    }
-  }
-  return data;
-}
-
-// function useEventSource(url: string, keyword: string) {
-//   const [data, setData] = useState("");
-
-//   useEffect(() => {
-//     if (keyword) {
-//       fetch(url, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ question: keyword }),
-//       }).then((response) => {
-//         const contentType = response.headers.get("Content-Type");
-//         if (response.body && contentType && contentType.includes("application/json")) {
-//           const reader = response.body.getReader();
-//           const decoder = new TextDecoder();
-
-//           function readChunk(): any {
-//             return reader.read().then(({ value, done }) => {
-//               if (done) return;
-
-//               const chunk = decoder.decode(value || new Uint8Array(), { stream: !done });
-//               const jsonData = chunk.slice(6);
-//               const result = JSON.parse(jsonData);
-
-//               console.log("이건 자른 데이터", jsonData);
-//               console.log("이건 자른 데이터 json화", result);
-
-//               if (result && result.choices && result.choices[0] && result.choices[0].delta) {
-//                 setData((prevData) => prevData + result.choices[0].delta.content);
-//               }
-
-//               return readChunk();
-//             });
-//           }
-
-//           return readChunk();
+//         let chunk = decoder.decode(value || new Uint8Array(), { stream: !done });
+//         chunk = chunk.slice(5); // 앞에 data: 자름
+//         const result = JSON.parse(chunk);
+//         if (chunk) {
+//           console.log("chunk", chunk);
+//         }
+//         if (result && result.choices && result.choices[0]) {
+//           data += result.choices[0].delta.content;
+//           console.log("dd", data);
+//           console.log("result", result);
+//           console.log("chunk", chunk);
 //         } else if (contentType && contentType.includes("text/html")) {
 //           console.log("json 타입이 아님 html임 stream 하지마셈");
 //         } else {
 //           throw new Error(`Unexpected content type: ${contentType}`);
 //         }
-//       });
-//     }
-//   }, []);
-
-//   return data;
-// }
-
-//     .then((response) => {
-//       if (response.body) {
-//         const reader = response.body.getReader();
-//         const decoder = new TextDecoder();
-
-//         function readChunk(): any {
-//           return reader.read().then(({ value, done }) => {
-//             if (done) return;
-
-//             const chunk = decoder.decode(value || new Uint8Array(), { stream: !done });
-//             const jsonData = chunk.slice(6);
-//             const result = JSON.parse(jsonData);
-
-//             console.log("이건 자른 데이터", jsonData);
-//             console.log("이건 자른 데이터 json화", result);
-
-//             if (result && result.choices && result.choices[0] && result.choices[0].delta) {
-//               setData((prevData) => prevData + result.choices[0].delta.content);
-//             }
-
-//             return readChunk();
-//           });
-//         }
-
-//         return readChunk();
 //       }
-//     });
-//   }, [url, keyword]);
-
+//     }
+//   }
 //   return data;
 // }
 
 export default function GPT() {
   const [keyword, setKeyword] = useState("");
-  const [enabled, setEnabled] = useState(false);
   const { getGPT } = useGPT();
   const [results, setResults] = useState("");
-  // const data = useEventSource("/api/gpt/chat");
-  // const data = useEventSource("/api/gpt/chat", keyword);
+
+  const fetchSSE = (url: string, keyword: string) => {
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({ question: keyword }),
+    })
+      .then((response) => {
+        const reader = response.body!.getReader();
+        const decoder = new TextDecoder();
+
+        const readChunk = (): any => {
+          return reader.read().then(appendChunks);
+        };
+
+        const appendChunks = (result: any) => {
+          const chunk = decoder
+            .decode(result.value || new Uint8Array(), {
+              stream: !result.done,
+            })
+            .replace(/^data: /, "");
+
+          try {
+            const parseData = JSON.parse(chunk);
+            const content = parseData.choices && parseData.choices[0].delta.content;
+
+            if (content) {
+              setResults((prevResults) => prevResults + content);
+            }
+          } catch (error) {
+            console.log("파싱 에러:", error);
+          }
+          if (!result.done) {
+            return readChunk();
+          }
+        };
+
+        return readChunk();
+      })
+      .then(() => {
+        // 종료 시 할 일
+      })
+      .catch((e) => {
+        // 에러 처리
+      });
+  };
 
   const handleEnter = async () => {
     console.log("지피티 질문", keyword);
-    const data = await fetchEventSource("/api/gpt/chat", keyword);
+    // const data = await fetchSSE("/api/gpt/chat", keyword);
+    fetchSSE("/api/gpt/chat", keyword);
 
     try {
       const response = await getGPT(keyword);
       console.log("GPT 응답", response);
 
-      setResults(data);
+      // setResults(data);
     } catch (err) {
       console.log(err);
     }
   };
-
-  useEffect(() => {
-    if (enabled) {
-      // refetch();
-      // setEnabled(false);
-    }
-  }, [enabled]);
 
   const onChange = (e: any) => {
     setKeyword(e.target.value);
