338f1d96637807f1d2922f5e669b6e330b223ffb
최상익 <qjxltm@naver.com>
2023-11-13 09:27:02 +0900

    Fix: 에디터와 멤버 fegin 코드 정리
    Change-Id: Id702d58c7d91bfd959b522d2643f08af05230917

--- diagram/src/main/java/com/surf/diagram/diagram/fegin/MemberServiceFeignClient.java
+++ diagram/src/main/java/com/surf/diagram/diagram/fegin/MemberServiceFeignClient.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name="server-auth", url="https://k9e101.p.ssafy.io")
 public interface MemberServiceFeignClient {
+    // pk 보내서 user 정보 받기
     @GetMapping("/api/auth/user-info/userpk/{userPk}")
     UserInfoResponseDto userInfoByUserPk(@PathVariable int userPk);
 }
