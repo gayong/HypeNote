f5060958d824e855a14979c1fe7a031f9236e88b
권인식 <kwon2566@naver.com>
2023-11-14 21:09:11 +0900

    Feat: Gpt Feign Connection Complete

--- gpt/src/main/java/com/gpt/gpt/controller/GptApiController.java
+++ gpt/src/main/java/com/gpt/gpt/controller/GptApiController.java
@@ -4,9 +4,7 @@ import com.gpt.gpt.dto.GptClientRequestDto;
 import com.gpt.gpt.service.GptApiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 
 @Slf4j
 @RestController
@@ -16,8 +14,8 @@ public class GptApiController {
 
     private final GptApiService gptApiService;
 
-    @PostMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> getChatGptResponse(@RequestBody GptClientRequestDto gptClientRequestDto) {
+    @PostMapping("/chat")
+    public String getChatGptResponse(@RequestBody GptClientRequestDto gptClientRequestDto) {
         return gptApiService.getChatGptResponse(gptClientRequestDto);
     }
 }
--- gpt/src/main/java/com/gpt/gpt/service/GptApiService.java
+++ gpt/src/main/java/com/gpt/gpt/service/GptApiService.java
@@ -2,12 +2,10 @@ package com.gpt.gpt.service;
 
 import com.gpt.gpt.dto.GptClientRequestDto;
 import com.gpt.gpt.dto.GptRequestDto;
-//import com.gpt.gpt.feign.GptFeignClient;
+import com.gpt.gpt.feign.GptFeignClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -17,18 +15,17 @@ import java.util.Map;
 @Service
 public class GptApiService {
 
-//    private final GptFeignClient gptFeignClient;
-    private final WebClient client = WebClient.create("https://api.openai.com/v1/");
+    private final GptFeignClient gptFeignClient;
 
     @Value("${apikey}")
     private String API_Key;
 
-    public Flux<String> getChatGptResponse(GptClientRequestDto question) {
+    public String getChatGptResponse(GptClientRequestDto question) {
 
         GptRequestDto requestBody = new GptRequestDto();
         requestBody.setModel("gpt-3.5-turbo");
 
-        requestBody.setStream(true);
+//        requestBody.setStream(true);
 
         Map<String, String> message = new HashMap<>();
         message.put("role", "system");
@@ -40,12 +37,14 @@ public class GptApiService {
 
         requestBody.setMessages(Arrays.asList(message, userMessage));
 
-        return client.post()
-                .uri("/chat/completions")
-                .header("Authorization", API_Key)
-                .header("Content-Type", "application/json")
-                .bodyValue(requestBody)
-                .retrieve()
-                .bodyToFlux(String.class);
+        return gptFeignClient.generateCompletion(API_Key, requestBody);
+
+//        return client.post()
+//                .uri("/chat/completions")
+//                .header("Authorization", API_Key)
+//                .header("Content-Type", "application/json")
+//                .bodyValue(requestBody)
+//                .retrieve()
+//                .bodyToFlux(String.class);
     }
 }
