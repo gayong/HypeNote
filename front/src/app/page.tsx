5eb1703369d39e4eb01c59f09d20cddbcd145db6
이가영 <lgy4625@naver.com>
2023-11-14 14:42:04 +0900

    Fix : 인트로 css
    Change-Id: Iea22b2671c992f63d93821e98b9145bf6553e849

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -2,7 +2,7 @@ import Intro from "@/components/intro/Intro";
 
 export default function Home() {
   return (
-    <div className="h-[5352px] bg-primary">
+    <div className="h-[5352px] bg-primary scrollbar-hide">
       <Intro />
     </div>
   );
