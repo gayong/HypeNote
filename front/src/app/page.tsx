42c4b8d179e9544cce21f3f99394510b086f540b
이가영 <lgy4625@naver.com>
2023-11-14 12:34:34 +0900

    Feat : 라우터 변경
    Change-Id: I43928e11c2d1d059162390f0e72a4b62dc3b5a30

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,25 +1,9 @@
-import ThreeScene from "@/components/brain/test";
-import loading from "../../public/assets/loading.gif";
-import { Suspense } from "react";
-import Image from "next/image";
-// import { cookies } from "next/headers";
+import Intro from "@/components/intro/Intro";
 
 export default function Home() {
-  // const cookiesList = cookies();
-  // const refreshToken = cookiesList.get("refreshToken");
-  // console.log("GDGDG");
-  // console.log("@@@", refreshToken);
-
   return (
-    <div className="flex h-screen max-h-screen scrollbar-hide">
-      <Suspense
-        fallback={
-          <div className="flex mx-auto justify-center items-center">
-            <Image src={loading} alt="로딩" priority />
-          </div>
-        }>
-        {/* <ThreeScene /> */}
-      </Suspense>
+    <div className="h-[5352px] bg-primary">
+      <Intro />
     </div>
   );
 }
