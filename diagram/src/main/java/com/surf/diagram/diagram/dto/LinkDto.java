d6418ffcb397692e62e8d4d417b99212d28e79b2
최상익 <qjxltm@naver.com>
2023-11-15 09:57:28 +0900

    Fix: 링크 dto 수정
    Change-Id: Ia52b41ebe739d9abc982c410608840334f0265db

--- diagram/src/main/java/com/surf/diagram/diagram/dto/LinkDto.java
+++ diagram/src/main/java/com/surf/diagram/diagram/dto/LinkDto.java
@@ -4,6 +4,8 @@ package com.surf.diagram.diagram.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 
 @Getter
 @Setter
@@ -14,4 +16,18 @@ public class LinkDto {
     private String target;
     private double similarity;
     private int userId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LinkDto linkDto = (LinkDto) o;
+        return (source.equals(linkDto.source) && target.equals(linkDto.target)) ||
+                (source.equals(linkDto.target) && target.equals(linkDto.source));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(source, target) + Objects.hash(target, source);
+    }
 }
