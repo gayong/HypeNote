6aef9140bf4ce9f0f4101c272282d71e3f21058e
이가영 <lgy4625@naver.com>
2023-11-16 13:55:04 +0900

    Feat : 레이아웃 수정중 퀴즈

--- front/src/components/ui/chat.tsx
+++ front/src/components/ui/chat.tsx
@@ -11,13 +11,11 @@ export function YourChat(props: chatUser) {
         </div>
       </div>
       <div>
-        <div className="flex justify-start">
-          <p className="text-xs text-right">{props.userName}</p>
-          <span className="w-full text-xs leading-none text-right block mt-1">{formatTime(props.chatTime)}</span>
-        </div>
+        <p className="text-xs text-right">{props.userName}</p>
         <div className="bg-primary text-font_primary p-2 rounded-r-lg rounded-bl-lg">
           <p className="text-sm">{props.content}</p>
         </div>
+        <span className="w-full text-xs leading-none text-right block mt-1">{formatTime(props.chatTime)}</span>
       </div>
     </div>
   );
@@ -27,13 +25,11 @@ export function MyChat(props: chatUser) {
   return (
     <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end items-start">
       <div>
-        <div className="flex justify-end">
-          <p className="text-xs text-right">{props.userName}</p>
-          <span className="w-full text-xs leading-none text-right block mt-1">{formatTime(props.chatTime)}</span>
-        </div>
+        <p className="text-xs text-right">{props.userName}</p>
         <div className="bg-line_primary text-font_primary text-white p-2 rounded-l-lg rounded-br-lg">
           <p className="text-sm">{props.content}</p>
         </div>
+        <span className="w-full text-xs leading-none text-right block mt-1">{formatTime(props.chatTime)}</span>
       </div>
       <div className="flex flex-col items-center justify-center flex-shrink-0 h-15 w-15">
         <div className="h-[50px] w-[50px] flex items-center justify-center">
