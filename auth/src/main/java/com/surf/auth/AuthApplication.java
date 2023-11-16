ebab84cd60f4d29f22af41c7e7f6a4fbae3e370b
권인식 <kwon2566@naver.com>
2023-11-16 11:14:54 +0900

    Feat: shared Root Delete Service typo
    user.getSharedDocumentsRoots

--- auth/src/main/java/com/surf/auth/AuthApplication.java
+++ auth/src/main/java/com/surf/auth/AuthApplication.java
@@ -2,12 +2,12 @@ package com.surf.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableWebSecurity
 @SpringBootApplication
