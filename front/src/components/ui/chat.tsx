1d4281593b3963111f4e28748a44c5de12f4acd5
이가영 <lgy4625@naver.com>
2023-11-16 12:51:10 +0900

    Fix : 먼저풀고 기다릴때 채팅, 채팅 년월일 삭제, gpt 띄우는 속도 빠르게

--- front/src/components/ui/chat.tsx
+++ front/src/components/ui/chat.tsx
@@ -1,5 +1,7 @@
 import { chatUser } from "@/types/quiz";
 
+const formatTime = (datetime: string) => datetime.slice(11);
+
 export function YourChat(props: chatUser) {
   return (
     <div className="flex w-full mt-2 space-x-3 max-w-xs">
@@ -9,12 +11,13 @@ export function YourChat(props: chatUser) {
         </div>
       </div>
       <div>
-        <p className="text-xs text-left">{props.userName}</p>
-
+        <div className="flex justify-start">
+          <p className="text-xs text-right">{props.userName}</p>
+          <span className="w-full text-xs leading-none text-right block mt-1">{formatTime(props.chatTime)}</span>
+        </div>
         <div className="bg-primary text-font_primary p-2 rounded-r-lg rounded-bl-lg">
           <p className="text-sm">{props.content}</p>
         </div>
-        <span className="w-full text-xs text-left block mt-1 leading-none">{props.chatTime}</span>
       </div>
     </div>
   );
@@ -24,12 +27,13 @@ export function MyChat(props: chatUser) {
   return (
     <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end items-start">
       <div>
-        <p className="text-xs text-right">{props.userName}</p>
-
+        <div className="flex justify-end">
+          <p className="text-xs text-right">{props.userName}</p>
+          <span className="w-full text-xs leading-none text-right block mt-1">{formatTime(props.chatTime)}</span>
+        </div>
         <div className="bg-line_primary text-font_primary text-white p-2 rounded-l-lg rounded-br-lg">
           <p className="text-sm">{props.content}</p>
         </div>
-        <span className="w-full text-xs leading-none text-right block mt-1">{props.chatTime}</span>
       </div>
       <div className="flex flex-col items-center justify-center flex-shrink-0 h-15 w-15">
         <div className="h-[50px] w-[50px] flex items-center justify-center">
